94ef7424235064bbd0b4cf0431996d34f140aaa1
Rutuja Salunkhe <115962915+rutujarsalunkhe@users.noreply.github.com>
2025-05-11 12:56:08 +0530

    Delete employee-management-master/employee-management-master directory

--- employee-management-master/employee-management-master/libs/sessionHelper.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (isset($_SESSION['logged'])) {
-   if (((time() - $_SESSION['logTime']) > 600) || isset($_POST['logout'])) {
-      header("Location: ". BASE_URL ."/loginController/logout/");
-   }
-}
